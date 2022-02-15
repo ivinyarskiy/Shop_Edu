7e59524ab7d2e6bc6cb6e3ceb9a0b1e41cd6004f
ivinyarskiy <ivinyarskiy@gmail.com>
2022-02-15 12:11:30 +0300

    C:/Program Files/Git/

---
+++ ShopEduApp/ShopEduApp/Models/Goods.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopEduApp
+{
+    public class Goods
+    {
+        public int GoodsId { get; set; }
+        public string GoodsName { get; set; }
+        public string GoodsBrand { get; set; }
+        public string GoodsModel { get; set; }
+        public string GoodsCountry { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomersId { get; set; }
+        public string CustomersName { get; set; }
+        public string CustomersSurname { get; set; }
+        public string CustomersSecondName { get; set; }
+        public string CustomersAddress { get; set; }
+        public string CustomersEmail { get; set; }
+        public string CustomersPhone { get; set; }
+    }
+
+    public class Payments
+    {
+        public int PaymentId { get; set; }
+        public int OrderId { get; set; }
+        
+        public DateTime { get; set; }
+        public string CustomersSurname { get; set; }
+        public string CustomersSecondName { get; set; }
+        public string CustomersAddress { get; set; }
+        public string CustomersEmail { get; set; }
+        public string CustomersPhone { get; set; }
+    }
+
+}
