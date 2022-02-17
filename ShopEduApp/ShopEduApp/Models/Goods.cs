b1df9b1c653f26f987270b14dba3e2d7a572f050
ivinyarskiy <ivinyarskiy@gmail.com>
2022-02-17 23:38:35 +0300

    goodsModified

--- ShopEduApp/ShopEduApp/Models/Goods.cs
+++ ShopEduApp/ShopEduApp/Models/Goods.cs
@@ -6,10 +6,15 @@ namespace ShopEduApp
     {
         [Key]
         public int GoodsId { get; set; }
+        [Display(Name = "Наименование")]
         public string GoodsName { get; set; }
+        [Display(Name = "Производитель")] 
         public string GoodsBrand { get; set; }
+        [Display(Name = "Модель")] 
         public string GoodsModel { get; set; }
+        [Display(Name = "Страна производства")] 
         public string GoodsCountry { get; set; }
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
     }
 
@@ -17,16 +22,23 @@ namespace ShopEduApp
     {
         [Key]
         public int CustomersId { get; set; }
+        [Display(Name = "Имя")]
         public string CustomersName { get; set; }
+        [Display(Name = "Фамилия")]
         public string CustomersSurname { get; set; }
+        [Display(Name = "Отчество (при наличии)")]
         public string CustomersSecondName { get; set; }
+        [Display(Name = "Адрес")]
         public string CustomersAddress { get; set; }
+        [Display(Name = "Email")]
         public string CustomersEmail { get; set; }
+        [Display(Name = "Телефон")] 
         public string CustomersPhone { get; set; }
     }
 
     public class Payments
     {
+        [Key]
         public int PaymentId { get; set; }
         public int OrderId { get; set; }
 
